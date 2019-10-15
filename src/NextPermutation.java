eccf42b621a2f4c9ea07fd908f3caf84f172d421
pengfei.cheng <pengfei.cheng@yeepay.com>
2019-10-15 19:01:07 +0800

    代码提交

--- src/NextPermutation.java
+++ src/NextPermutation.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * @author pengfei.cheng
  * 31. 下一个排列
@@ -6,21 +8,34 @@
 public class NextPermutation {
     /**
      * 实现获取下一个排列的函数，算法需要将给定数字序列重新排列成字典序中下一个更大的排列。
-     *
+     * <p>
      * 如果不存在下一个更大的排列，则将数字重新排列成最小的排列（即升序排列）。
-     *
+     * <p>
      * 必须原地修改，只允许使用额外常数空间。
-     *
+     * <p>
      * 以下是一些例子，输入位于左侧列，其相应输出位于右侧列。
      * 1,2,3 → 1,3,2
      * 3,2,1 → 1,2,3
      * 1,1,5 → 1,5,1
-     *
+     * <p>
      * 来源：力扣（LeetCode）
      * 链接：https://leetcode-cn.com/problems/next-permutation
      * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
      */
     public void nextPermutation(int[] nums) {
-
+        if (nums.length <= 1) {
+            return;
+        }
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                int tmp = nums[i + 1];
+                nums[i + 1] = nums[i];
+                nums[i] = tmp;
+                break;
+            }
+            if (i == 0) {
+                Arrays.sort(nums);
+            }
+        }
     }
 }
