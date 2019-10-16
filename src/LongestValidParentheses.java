ac5b88b3f261d74f66bb9398d78e043d408382ed
pengfei.cheng <pengfei.cheng@yeepay.com>
2019-10-16 17:50:57 +0800

    代码提交

--- src/LongestValidParentheses.java
+++ src/LongestValidParentheses.java
@@ -76,6 +76,25 @@ public class LongestValidParentheses {
     }
 
     public static void main(String[] args) {
-        longestValidParentheses("(()(((()");
+        longestValidParentheses1("))))))))()");
+    }
+
+    public static int longestValidParentheses1(String s) {
+        int maxans = 0;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                if (stack.empty()) {
+                    stack.push(i);
+                } else {
+                    maxans = Math.max(maxans, i - stack.peek());
+                }
+            }
+        }
+        return maxans;
     }
 }
