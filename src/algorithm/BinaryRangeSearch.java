1a02328f773f08e00b33ae68d4af3b560e5a9c31
pengfei.cheng <pengfei.cheng@yeepay.com>
2019-10-17 11:10:31 +0800

    代码提交

---
+++ src/algorithm/BinaryRangeSearch.java
@@ -0,0 +1,9 @@
+package algorithm;
+
+/**
+ * @author pengfei.cheng
+ * 二分范围查找
+ * @since 2019/10/17 上午11:10
+ */
+public class BinaryRangeSearch {
+}
---
+++ src/algorithm/basic/BinarySearch.java
@@ -0,0 +1,67 @@
+package algorithm.basic;
+
+/**
+ * @author pengfei.cheng
+ * 二分查找
+ * @since 2019/10/17 上午10:12
+ */
+public class BinarySearch {
+
+    /**
+     * 二分查找
+     *
+     * @param sortArray
+     * @param element
+     */
+    public static int binarySearch(int[] sortArray, int element) {
+        return recursiveBinarySearch(sortArray, element);
+    }
+
+    /**
+     * 二分查找（迭代实现）
+     */
+    private static int iterateBinarySearch(int[] sortArray, int element) {
+        int last = 0;
+        int next = sortArray.length - 1;
+        int middle;
+        int i = 0;
+        while (element != (sortArray[(middle = (last + next) / 2)])) {
+            if (last == next) {
+                return -1;
+            }
+            if (element > (sortArray[middle])) {
+                last = middle;
+            } else {
+                next = middle / 2;
+            }
+        }
+        return middle;
+    }
+
+    /**
+     * 二分查找（递归实现）
+     */
+    private static int recursiveBinarySearch(int[] sortArray, int element) {
+        return recursiveBinarySearch(sortArray, element, 0, sortArray.length - 1);
+    }
+
+    private static int recursiveBinarySearch(int[] sortArray, int element, int start, int end) {
+        int middle;
+        if (element == sortArray[(middle = (start + end) / 2)]) {
+            return middle;
+        }
+        if (start == end) {
+            return -1;
+        }
+        if (element > (sortArray[middle])) {
+            start = middle;
+        } else {
+            end = middle / 2;
+        }
+        return recursiveBinarySearch(sortArray, element, start, end);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(binarySearch(new int[]{1, 2, 3, 4, 5, 6}, 4));
+    }
+}
