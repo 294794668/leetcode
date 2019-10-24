752bb99d9958772120a2671fe94f4b020803eb48
pengfei.cheng <pengfei.cheng@yeepay.com>
2019-10-24 18:29:03 +0800

    代码提交

--- src/RegularExpressionMatching.java
+++ src/RegularExpressionMatching.java
@@ -68,16 +68,12 @@ public class RegularExpressionMatching {
         while ((ar >= 0 && br >= 0) && (a[ar] == b[br] || b[br] == '.')) {
             ar--;
             br--;
-            if (ar == -1 || br == -1) {
-                break;
-            }
         }
-        if (ar == -1 && br == -1) {
-            return true;
+        //字符规律匹配完，字符匹配完成则匹配成功 否则失败
+        if (br == -1) {
+            return ar == -1;
         }
-        if (ar >= 0 && br == -1) {
-            return false;
-        }
+        //字符规律最后一位不是*，匹配失败
         if (b[br] != '*') {
             return false;
         }
@@ -85,7 +81,9 @@ public class RegularExpressionMatching {
     }
 
     private static boolean isMatch(char[] a, int al, int ar, char[] b, int bl, int br) {
+        //a全部匹配结束
         if (al > ar) {
+            //b匹配没有结束，则剩余结构必须是（字母*）或（*字母*）
             if (bl < br) {
                 int j = '*' == b[bl] ? 0 : 1;
                 for (int i = bl + j; i <= br; i += 2) {
@@ -94,21 +92,27 @@ public class RegularExpressionMatching {
                     }
                 }
             }
+            //b也匹配结束
             return true;
         }
+        //a没结束b结束了。。。失败啊
         if (bl > br) {
             return false;
         }
         if (a[al] == b[bl] || b[bl] == '.') {
+            //匹配成功a b都向后移动一位
             return isMatch(a, ++al, ar, b, ++bl, br);
         } else if (b[bl] == '*') {
+            //遇到匹配0或多，出现以下情况
             //1.停止匹配
             //2.继续匹配
-            //3.忽略上一个匹配 0匹配
+            //3.忽略上一个匹配 0匹配————似乎出现重复
             int bl1 = bl + 1;
             int bl2 = bl - 1;
             return isMatch(a, al, ar, b, bl1, br) || isMatch(a, al, ar, b, bl2, br) || isMatch(a, --al, ar, b, bl1, br);
         } else {
+            //不匹配但是后面是* 可以跳过
+            //最后一位肯定是*，所以不存在数组越界
             if (b[++bl] == '*') {
                 return isMatch(a, al, ar, b, ++bl, br);
             }
