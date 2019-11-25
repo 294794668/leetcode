136dbd8ca5b133247e46e5b1c17a7f0878f9dbb9
pengfei.cheng <pengfei.cheng@yeepay.com>
2019-11-25 17:41:40 +0800

    代码提交

--- src/Permutations.java
+++ src/Permutations.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author pengfei.cheng
@@ -68,4 +65,41 @@ public class Permutations {
         Permutations p = new Permutations();
         p.permute(new int[]{1, 2, 3, 4, 5});
     }
+
+
+    public List<List<Integer>> permute1(int[] nums) {
+        int len = nums.length;
+
+        List<List<Integer>> res = new ArrayList<>();
+        if (len == 0) {
+            return res;
+        }
+
+        // 使用哈希表检测一个数字是否使用过
+        Set<Integer> used = new HashSet<>();
+        Stack<Integer> stack = new Stack<>();
+
+        backtrack(nums, 0, len, used, stack, res);
+        return res;
+    }
+
+    private void backtrack(int[] nums, int depth, int len, Set<Integer> used, Stack<Integer> stack, List<List<Integer>> res) {
+        if (depth == len) {
+            res.add(new ArrayList<>(stack));
+            return;
+        }
+
+        for (int i = 0; i < len; i++) {
+            if (!used.contains(i)) {
+                used.add(i);
+                stack.push(nums[i]);
+
+                backtrack(nums, depth + 1, len, used, stack, res);
+
+                stack.pop();
+                used.remove(i);
+            }
+        }
+    }
+
 }
