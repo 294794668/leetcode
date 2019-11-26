d7214a50473fed4bc24e5794d825779d6c62bc13
pengfei.cheng <pengfei.cheng@yeepay.com>
2019-11-26 16:19:20 +0800

    代码提交 最近脑子不够用了。。

---
+++ src/GroupAnagrams.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+/**
+ * @author pengfei.cheng
+ * 49.字母异位词分组
+ * @since 2019/11/26 上午11:23
+ */
+public class GroupAnagrams {
+    /**
+     * 给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。
+     * <p>
+     * 示例:
+     * <p>
+     * 输入: ["eat", "tea", "tan", "ate", "nat", "bat"],
+     * 输出:
+     * [
+     * ["ate","eat","tea"],
+     * ["nat","tan"],
+     * ["bat"]
+     * ]
+     * <p>
+     * 说明：
+     * <p>
+     * 所有输入均为小写字母。
+     * 不考虑答案输出的顺序。
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/group-anagrams
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String key = String.valueOf(chars);
+            if (!map.containsKey(key)) map.put(key, new ArrayList());
+            map.get(key).add(str);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
--- src/PermutationsII.java
+++ src/PermutationsII.java
@@ -7,6 +7,7 @@ import java.util.*;
  */
 public class PermutationsII {
     /**
+     * todo 暂时想不出来
      * 给定一个可包含重复数字的序列，返回所有不重复的全排列。
      * <p>
      * 示例:
