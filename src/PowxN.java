985a97973c7471618eb59c2fe958b9cac256771a
pengfei.cheng <pengfei.cheng@yeepay.com>
2019-11-27 14:45:50 +0800

    代码提交

--- src/PowxN.java
+++ src/PowxN.java
@@ -58,6 +58,21 @@ public class PowxN {
         return result;
     }
 
+
+    public static double myPowBest(double x, int n) {
+        double res = 1.0;
+
+        for (int i = n; i != 0; i /= 2) {
+
+            if (i % 2 != 0) {
+                res *= x;
+            }
+            x *= x;
+        }
+
+        return n < 0 ? 1 / res : res;
+    }
+
     public static void main(String[] args) {
         System.out.println(myPow(3, 3));
     }
