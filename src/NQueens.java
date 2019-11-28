5ae7cc1037752c2d2110b654fea636f61fe6dede
pengfei.cheng <pengfei.cheng@yeepay.com>
2019-11-28 14:34:02 +0800

    代码提交

--- src/NQueens.java
+++ src/NQueens.java
@@ -84,4 +84,52 @@ public class NQueens {
     public static void main(String[] args) {
         System.out.println(new NQueens().solveNQueens(4));
     }
+
+
+    class Solution {
+        private List<List<String>> solutions;
+        private char[][] array;
+        private boolean[] colUsed;
+        private boolean[] diagonals45Used;
+        private boolean[] diagonals135Used;
+        private int n;
+
+        public List<List<String>> solveNQueens(int n) {
+            solutions = new ArrayList<>();
+            array = new char[n][n];
+            for (char[] chars : array) {
+                Arrays.fill(chars, '.');
+            }
+            colUsed = new boolean[n];
+            diagonals45Used = new boolean[2 * n - 1];
+            diagonals135Used = new boolean[2 * n - 1];
+            this.n = n;
+            backtracking(0);
+            return solutions;
+        }
+
+        private void backtracking(int row) {
+            if (row == n) {
+                List<String> list = new ArrayList<>();
+                for (char[] chars : array) {
+                    list.add(new String(chars));
+                }
+                solutions.add(list);
+                return;
+            }
+            for (int col = 0; col < n; col++) {
+                int diagonals45Idx = col + row;
+                int diagonals135Idx = n - 1 - (row - col);
+                if (colUsed[col] || diagonals135Used[diagonals135Idx] || diagonals45Used[diagonals45Idx]) {
+                    continue;
+                }
+                colUsed[col] = diagonals135Used[diagonals135Idx] = diagonals45Used[diagonals45Idx] = true;
+                array[row][col] = 'Q';
+                backtracking(row + 1);
+                array[row][col] = '.';
+                colUsed[col] = diagonals135Used[diagonals135Idx] = diagonals45Used[diagonals45Idx] = false;
+            }
+        }
+    }
+
 }
